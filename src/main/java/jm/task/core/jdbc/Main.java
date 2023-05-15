9501859f60afc2b224201083b01bca83152de7a6
Salavat1994 <salavatcamaletdinov1994@gmail.com>
2023-05-15 23:46:31 +0300

    П.П_1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
+import static com.sun.xml.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl table = new UserServiceImpl();
+        table.createUsersTable();
+
+        table.saveUser("Salavat", "Kamaletdinov", (byte) 28);
+        table.saveUser("Oleg", "Petrov", (byte) 27);
+        table.saveUser("Ivan", "Ivanov", (byte) 26);
+
+        List<User> list = table.getAllUsers();
+        for (User user : list){
+            System.out.println(user);
+        }
+        table.removeUserById(2);
+        table.cleanUsersTable();
+        table.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String username = "postgres";
+    public static final String userpassword = "root";
+    public static final String driver_class = "org.postgresql.Driver";
+
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory sessionFactory() throws SQLException {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.driver_class", driver_class);
+            configuration.setProperty("hibernate.connection.url", url);
+            configuration.setProperty("hibernate.connection.username", username);
+            configuration.setProperty("hibernate.connection.password", userpassword);
+            configuration.addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
