0ccdaf6a8efb3b09616559948df2cbd84e85756e
Salavat1994 <salavatcamaletdinov1994@gmail.com>
2023-05-17 22:02:02 +0300

    Поменял имя переменной, добавил таргет в гитИгнор

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,25 +5,25 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
-import static com.sun.xml.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl table = new UserServiceImpl();
-        table.createUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
 
-        table.saveUser("Salavat", "Kamaletdinov", (byte) 28);
-        table.saveUser("Oleg", "Petrov", (byte) 27);
-        table.saveUser("Ivan", "Ivanov", (byte) 26);
+        userService.saveUser("Salavat", "Kamaletdinov", (byte) 28);
+        userService.saveUser("Oleg", "Petrov", (byte) 27);
+        userService.saveUser("Ivan", "Ivanov", (byte) 26);
 
-        List<User> list = table.getAllUsers();
+        List<User> list = userService.getAllUsers();
         for (User user : list){
             System.out.println(user);
         }
-        table.removeUserById(2);
-        table.cleanUsersTable();
-        table.dropUsersTable();
+        userService.removeUserById(2);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
