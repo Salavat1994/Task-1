01cddf8d43f36337e32244b4a0fd1425625b2c1e
Salavat1994 <salavatcamaletdinov1994@gmail.com>
2023-05-22 16:15:11 +0300

    Commit_1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
